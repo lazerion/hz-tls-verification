5956549a7f72b9289acb7bb63db4b5ebc6a84a25
baris <baris.aydinoz@gmail.com>
2017-12-22 20:38:21 +0300

    added deployment cases after review

--- hz/ks/create-key.sh
+++ hz/ks/create-key.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-#KEYSTORE_PASSWORD=s3cr3t
-#KEYTOOL_CMD="keytool -genkeypair -keystore ks.p12 -storepass $KEYSTORE_PASSWORD -keypass $KEYSTORE_PASSWORD -storetype PKCS12 -validity 7300 -keyalg RSA -keysize 1024"
-#
-#$KEYTOOL_CMD -alias cn-ip -dname "uid=cn-is-not-first, CN=127.0.0.1, O=Hazelcast"
+# This script generates keystores which are used in TLS host verification tests
+# Once we move out from Java 6 support, we can generate the key material by using a Maven keytool plugin.
+# KeyTool in Java 6 doesn't support the SAN extenxion.
 
-keytool -genkey -alias hz -keyalg RSA -keystore keystore.jks -validity 360 -dname "CN=172.20.1.1/255.255.0.0"
-keytool -genkey -alias hz -keyalg RSA -keystore keystore-invalid.jks -validity 360 -dname "CN=192.20.1.1/255.255.0.0"
+KEYSTORE_PASSWORD=123456
+
+# key material for TlsHostVerifierTest.java
+
+function createKeystore
+{
+  if [ $# -gt 2 ]; then
+    keytool -genkeypair -keystore $1 -storepass $KEYSTORE_PASSWORD -keypass $KEYSTORE_PASSWORD -storetype JKS -validity 7300 -keyalg RSA -keysize 1024 \
+      -alias test -dname "$2" -ext "$3"
+  else
+    keytool -genkeypair -keystore $1 -storepass $KEYSTORE_PASSWORD -keypass $KEYSTORE_PASSWORD -storetype JKS -validity 7300 -keyalg RSA -keysize 1024 \
+      -alias test -dname "$2"
+  fi
+  keytool -export -alias test -keystore $1 -storepass $KEYSTORE_PASSWORD -file $1.crt
+  keytool -importcert -noprompt -alias $1 -keystore truststore.jks -storepass $KEYSTORE_PASSWORD -file $1.crt
+  rm $1.crt
+}
+
+createKeystore keystore.jks "cn=172.20.0.1/255.255.0.0"
